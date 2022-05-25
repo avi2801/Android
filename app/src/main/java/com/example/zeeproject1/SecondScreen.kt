a012708eaa38cfba5a72fb6503b399d3e244c4f7
Avinash Anand <avinash.anand@zee.com>
2022-05-25 13:06:48 +0530

    Third Screen Added

--- app/src/main/java/com/example/zeeproject1/SecondScreen.kt
+++ app/src/main/java/com/example/zeeproject1/SecondScreen.kt
@@ -1,12 +1,13 @@
 package com.example.zeeproject1
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second_screen.*
 
 class SecondScreen : AppCompatActivity() {
@@ -15,12 +16,23 @@ class SecondScreen : AppCompatActivity() {
         setContentView(R.layout.activity_second_screen)
         var user = intent.extras?.getString("username")
         textView1.setText(user)
+        myButton2.setOnClickListener {
+            var myIntent1 = Intent(this,ThirdScreen::class.java)
+            startActivity(myIntent1)
+        }
+
+
+        var actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
        menuInflater.inflate(R.menu.menu,menu)
        return super.onCreateOptionsMenu(menu)
     }
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         var id = item.itemId
 
@@ -34,6 +46,13 @@ class SecondScreen : AppCompatActivity() {
             var i = Intent(this,MainActivity::class.java)
             startActivity(i)
         }
+
+        if(id == android.R.id.home)
+        {
+           var i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+        }
+
 
         return super.onOptionsItemSelected(item)
     }
---
+++ app/src/main/java/com/example/zeeproject1/ThirdScreen.kt
@@ -0,0 +1,17 @@
+package com.example.zeeproject1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+class ThirdScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_screen)
+        var actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+   
+}
