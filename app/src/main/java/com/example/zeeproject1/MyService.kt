bc512789ccdf802202d751254ba8afc5a6a0e4d0
Avinash Anand <avinash.anand@zee.com>
2022-05-26 14:20:31 +0530

    Music added on the Splash Screen

--- app/src/main/java/com/example/zeeproject1/MyService.kt
+++ app/src/main/java/com/example/zeeproject1/MyService.kt
@@ -2,11 +2,28 @@ package com.example.zeeproject1
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
 
 class MyService : Service() {
+    lateinit var mp:MediaPlayer
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mp = MediaPlayer.create(this,R.raw.blindinglite)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        mp.start()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        mp.stop()
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/example/zeeproject1/SplashScreen.kt
+++ app/src/main/java/com/example/zeeproject1/SplashScreen.kt
@@ -1,9 +1,11 @@
 package com.example.zeeproject1
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
@@ -12,22 +14,30 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
         var h = Handler()
         Thread{
-//            Thread.sleep(5000)
-//            var i = Intent(this,MainActivity::class.java)
-//            startActivity(i)
-            var counter = 1
-            for(i in 1..5)
-            {
-                h.post{
-                   textView1.setText("" + counter)
-                }
+//            h.post{
+//                var i = Intent(this,MyService::class.java)
+//                startService(i)
+//
+//            }
+//            Thread.sleep(10000)
+//            h.post{
+//                var i = Intent(this,MyService::class.java)
+//                stopService(i)
+//            }
 
-                Thread.sleep(1000)
-                counter++
-            }
-            var i = Intent(this, MainActivity::class.java)
-            startActivity(i)
+            var i = Intent(this,MyService::class.java)
+            startService(i)
+
+            Thread.sleep(10000)
+
+            stopService(i)
+
+            var i3 = Intent(this,MainActivity::class.java)
+            startActivity(i3)
 
         }.start()
+
+
+
     }
 }
