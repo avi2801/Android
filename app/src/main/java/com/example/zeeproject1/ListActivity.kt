17aed06d1e20e73aa431114f1d6a50ccfae4bc86
Avinash Anand <avinash.anand@zee.com>
2022-05-26 16:30:21 +0530

    Location Added

--- app/src/main/java/com/example/zeeproject1/ListActivity.kt
+++ app/src/main/java/com/example/zeeproject1/ListActivity.kt
@@ -1,8 +1,11 @@
 package com.example.zeeproject1
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
@@ -15,6 +18,22 @@ class ListActivity : AppCompatActivity() {
 
         var arrayAdapter = ArrayAdapter(this,R.layout.mylayout,R.id.textView,countries)
         mylistView.adapter = arrayAdapter
+
+        mylistView.setOnItemClickListener{adapterView,view,i,pos->
+            var item = adapterView.getItemAtPosition(pos.toInt()).toString()
+            Toast.makeText(this,"you clicked on $item",Toast.LENGTH_SHORT).show()
+
+            val gmmIntentUri = Uri.parse("geo:0,0?q=${item}")
+
+// Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+// Make the Intent explicit by setting the Google Maps package
+            mapIntent.setPackage("com.google.android.apps.maps")
+
+// Attempt to
+            startActivity(mapIntent)
+
+        }
 
 
 
